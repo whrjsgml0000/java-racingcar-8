7839d5d05e0c9042064c83f4d68b329beff5f0bb
whrjsgml0000 <whrjsgml4515@naver.com>
2025-10-22 02:02:34 +0900

    test(input): InputParser 클래스 생성 및 관련 테스트 케이스 코드 작성, 테스트 케이스 csv 파일 작성

---
+++ src/main/java/racingcar/io/parser/InputParser.java
@@ -0,0 +1,14 @@
+package racingcar.io.parser;
+
+import java.util.List;
+
+public class InputParser {
+
+    public List<String> parseCarNames(String rawCarNames) {
+        return null;
+    }
+
+    public int parseTryCount(String rawTryCount) {
+        return 0;
+    }
+}
---
+++ src/test/java/racingcar/io/parser/InputParserTest.java
@@ -0,0 +1,67 @@
+package racingcar.io.parser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class InputParserTest {
+
+    InputParser inputParser = new InputParser();
+
+    @Nested
+    @DisplayName("차 이름 파싱 테스트")
+    class ParseCarNames {
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "차_이름_파싱_성공.csv")
+        void success(String rawCarNames, String rawResult) {
+            List<String> result = Arrays.stream(rawResult.split(",")).toList();
+            // given
+            // when
+            List<String> parsedCarNames = inputParser.parseCarNames(rawCarNames);
+
+            // then
+            assertThat(parsedCarNames).isEqualTo(result);
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "차_이름_파싱_실패.csv")
+        void failure(String rawCarNames, String messageKeyword) {
+            // given
+            // when & then
+            assertThatThrownBy(()->inputParser.parseCarNames(rawCarNames))
+                    .hasMessageContaining(messageKeyword);
+        }
+    }
+
+    @Nested
+    @DisplayName("시도 횟수 파싱 테스트")
+    class ParseTryCount {
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "시도_횟수_파싱_성공.csv")
+        void success(String rawTryCount, int result) {
+            // given
+            // when
+            int parsedTryCount = inputParser.parseTryCount(rawTryCount);
+
+            // then
+            assertThat(parsedTryCount).isEqualTo(result);
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "시도_횟수_파싱_실패.csv")
+        void failure(String rawTryCount, String messageKeyword) {
+            // given
+            // when & then
+            assertThatThrownBy(()->inputParser.parseTryCount(rawTryCount))
+                    .hasMessageContaining(messageKeyword);
+        }
+    }
+}
