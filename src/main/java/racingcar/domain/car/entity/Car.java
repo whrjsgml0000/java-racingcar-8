0147c059a10363c04124aa08d2f201133138bdcc
whrjsgml0000 <whrjsgml4515@naver.com>
2025-10-23 01:47:11 +0900

    feat(car): 동일한 이름을 가진 차를 같은 hash값을 갖도록 함.

--- src/main/java/racingcar/domain/car/entity/Car.java
+++ src/main/java/racingcar/domain/car/entity/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain.car.entity;
 
+import java.util.Objects;
+
 public class Car {
 
     private final String name;
@@ -20,5 +22,20 @@ public class Car {
 
     public enum State {
         GO, STOP
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Car car = (Car) o;
+        return Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
