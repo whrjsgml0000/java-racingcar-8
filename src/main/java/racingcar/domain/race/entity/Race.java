10e2365250e0ead1f8a69b18e741f2706647c3f2
whrjsgml0000 <whrjsgml4515@naver.com>
2025-10-23 10:59:09 +0900

    feat(race): 레이스 한 턴 진행 후 남은 시도 횟수 감소 기능 추가

--- src/main/java/racingcar/domain/race/entity/Race.java
+++ src/main/java/racingcar/domain/race/entity/Race.java
@@ -7,23 +7,27 @@ import racingcar.domain.race.dto.res.CurrentRaceStatusDTO;
 
 public class Race {
 
-    private static final int START_VALUE = 0;
-    private final Map<Car, Integer> registeredCars = new LinkedHashMap();
+    private static final int START_LINE = 0;
+    private final Map<Car, Integer> raceStatus = new LinkedHashMap();
     private int remainTryCount;
 
     public Race(int remainTryCount) {
         this.remainTryCount = remainTryCount;
     }
 
+    public boolean isEnd() {
+        return remainTryCount <= 0;
+    }
+
     public void registerCar(Car car) {
-        if (registeredCars.containsKey(car)) {
+        if (raceStatus.containsKey(car)) {
             throw new IllegalArgumentException("동일한 이름을 가진 차를 등록할 수 없습니다.");
         }
-        registeredCars.put(car, START_VALUE);
+        raceStatus.put(car, START_LINE);
     }
 
     public void runOnce() {
-
+        remainTryCount--;
     }
 
     public CurrentRaceStatusDTO getCurrentRaceStatus() {
--- src/test/java/racingcar/domain/race/entity/RaceTest.java
+++ src/test/java/racingcar/domain/race/entity/RaceTest.java
@@ -1,13 +1,18 @@
 package racingcar.domain.race.entity;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.car.entity.Car;
+import racingcar.domain.car.entity.Car.State;
 
 class RaceTest {
 
     @Test
+    @DisplayName("동일한 이름을 가진 차 등록 실패")
     void registerCar() {
         // given
         Race race = new Race(5);
@@ -19,11 +24,43 @@ class RaceTest {
 
         // when & then
         assertThatThrownBy(() -> race.registerCar(hello1))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("동일한");
     }
 
-    @Test
-    void runOnce() {
+    @Nested
+    @DisplayName("레이스 진행")
+    class RunOnce{
+
+        @Test
+        @DisplayName("1번 레이스 진행 후 종료 확인")
+        void runOnce() {
+            // given
+            Race race = new Race(1);
+            Car car = new Car("car1", () -> State.GO);
+            Car car1 = new Car("car2", () -> State.STOP);
+            race.registerCar(car);
+            race.registerCar(car1);
+
+            // when & then
+            assertDoesNotThrow(race::runOnce);
+            assertThat(race.isEnd()).isTrue();
+        }
+
+        @Test
+        @DisplayName("1번 레이스 진행 후 진행중 확인")
+        void runOnce1() {
+            // given
+            Race race = new Race(2);
+            Car car = new Car("car1", () -> State.GO);
+            Car car1 = new Car("car2", () -> State.STOP);
+            race.registerCar(car);
+            race.registerCar(car1);
+
+            // when & then
+            assertDoesNotThrow(race::runOnce);
+            assertThat(race.isEnd()).isFalse();
+        }
     }
 
     @Test
